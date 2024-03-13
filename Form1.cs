ecd46f3f54fd075e8cc784b8ac13e3471296b203
Leopoldo <leofuenntes@gmail.com>
2024-03-12 19:51:10 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAHere
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("Ingresa el numero de bigotes");
+            }
+            else
+            {
+                //pasar tipo sting a int
+                int nBi = int.Parse(textBox2.Text);
+
+                Gato cat = new Gato("SCAR", 5, nBi);
+                //Mostrar datos
+                textBox1.Text = cat.mostrargati();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Vaca
+
+                //pasar tipo sting a int
+                int Lit = int.Parse(textBox5.Text);
+                int Ps = int.Parse(textBox4.Text);
+                string nom = (textBox3.Text);
+                Vaca vac =new Vaca(nom, Lit, Ps);
+                //Mostrar datos
+                textBox1.Text = vac.mostrvaca();
+            //textBox1.Text = vac.CalcularCanComida();
+        }
+    }
+}
---
+++ Vaca.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFAHere
+{
+    internal class Vaca:Mamifero
+    {
+        private double LitLeche { get; set; }
+
+        /*
+        public Vaca() : base("", 0)
+        {
+            Console.WriteLine("Ingrese los litros de leche");
+            LitLeche = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el peso");
+            peso = double.Parse(Console.ReadLine());
+        }
+        */
+
+        public Vaca(string n, int p, int l) : base(n, p)
+        {
+            this.LitLeche = l;
+        }
+
+        /*
+
+        public void CalcularCanComida()
+        {
+            double kiloscomida = 0;
+            kiloscomida = peso / (2 * LitLeche);
+            Console.WriteLine("La cantidad de comida es:" + kiloscomida);
+
+        }
+        */
+        /*
+        public void mostrarvaca()
+        {
+            string nomb = "";
+            nomb = getNombre();
+            Console.Write("Ingresa el nombre de la vaca: ");
+            nomb = Console.ReadLine();
+
+            Console.WriteLine("Clase Vaca");
+            Console.WriteLine("El nombre de la Vaca es: " + nomb);
+            Console.WriteLine("Peso: " + peso);
+            Console.WriteLine("Litros de leche: " + LitLeche);
+        }
+        */
+ 
+
+        public string mostrvaca() 
+        {
+            string dat;
+            dat = "El nombre de la vaca es: " + getNombre() + Environment.NewLine;
+            dat += "El peso es: " + peso + " kilogramos" + Environment.NewLine;
+            dat += "Litros de leche: " + LitLeche + Environment.NewLine;
+            dat += "La Vaca come:" + peso / (2 * LitLeche)+ Environment.NewLine;
+            return dat;
+        }
+    }
+}
